285e130c33664b489ffbf10609f5c8b9636840f8
Integer Number <124754713+IntegerNumber01@users.noreply.github.com>
2023-02-08 08:40:14 -0800

    Create problem2.py
    Watch my video on YouTube for further explanation.

---
+++ problem2.py
@@ -0,0 +1,20 @@
+# Theorem: If it is possible to buy x, x+1,..., x+5 sets of McNuggets, for some x, then it is
+# possible to buy any number of McNuggets >= x, given that McNuggets come in 6, 9 and 20
+# packs.
+
+# Explain, in English, why this theorem is true
+
+
+# 50 + 1 = 51
+# 50 + 2 = 52
+# 50 + 3 = 53
+# 50 + 4 = 54
+# 50 + 5 = 55
+# 50 + 6 = 56
+# 50 + 7 = 50 + 1 + 6
+# 50 + 8 = 50 + 2 + 6
+# 50 + 9 = 50 + 3 + 6
+# 50 + 10 = 50 + 4 + 6
+# 50 + 11 = 50 + 5 + 6
+# 50 + 12 = 50 + 6 + 6
+# 50 + 13 = 50 + 1 + 6 + 6
